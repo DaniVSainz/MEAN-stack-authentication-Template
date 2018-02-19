e0a467585e216448d0c4612bb63279d11ca904e2
danivsainz <danivsainz@gmail.com>
2018-02-19 08:38:13 -0500

    Remove old files from angular-cli

--- node-bodybuilding/angular-src/src/app/workout/exercise-show/exercise_set.ts
+++
@@ -1,8 +0,0 @@
-export class Exercise_set {
-  weight: number;
-  reps: number;
-  rest: number;
-  user: string;
-  workout: string;
-  exercise: string;  
-}
--- node-bodybuilding/angular-src/src/app/workout/workout.ts
+++
@@ -1,9 +0,0 @@
-export class Workout {
-    name: string;
-    date: string;
-    _id: string;
-    exercises: any[];
-    sets:any[];
-    user: string;
-  }
-  
