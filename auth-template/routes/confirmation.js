74bed13f012077c9d58e84f2c00fd1f131c65eb4
danivsainz <danivsainz@gmail.com>
2018-02-19 17:35:14 -0500

    Create resend verification function

--- auth-template/routes/confirmation.js
+++ auth-template/routes/confirmation.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const passport = require('passport');
 const Token = require('../models/verificationToken')
 const User = require('../models/user');
+var crypto = require('crypto');
+var nodemailer = require('nodemailer');
 
 router.get('/:id', (req, res, next) => {
 
@@ -19,6 +21,30 @@ router.get('/:id', (req, res, next) => {
             user.save(function (err) {
                 if (err) { return res.status(500).send({ msg: err.message }); }
                 res.status(200).send("The account has been verified. Please log in.");
+            });
+        });
+    });
+})
+
+router.get('/resend/:email', (req,res,next) => {
+
+    User.findOne({ email: req.params.email }, function (err, user) {
+        if (!user) return res.status(400).send({ msg: 'We were unable to find a user with that email.' });
+        if (user.isVerified) return res.status(400).send({ msg: 'This account has already been verified. Please log in.' });
+ 
+        // Create a verification token, save it, and send email
+        var token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
+ 
+        // Save the token
+        token.save(function (err) {
+            if (err) { return res.status(500).send({ msg: err.message }); }
+ 
+            // Send the email
+            var transporter = nodemailer.createTransport({ service: 'gmail', auth: { user: process.env.userEmail, pass: process.env.userPass } });
+            var mailOptions = { from: 'no-reply@yourwebapplication.com', to: user.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n' };
+            transporter.sendMail(mailOptions, function (err) {
+                if (err) { return res.status(500).send({ msg: err.message }); }
+                res.status(200).send('A verification email has been sent to ' + user.email + '.');
             });
         });
     });
