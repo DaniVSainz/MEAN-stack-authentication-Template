38ec108a4169c474d2eba2d6344b632ad1221015
danivsainz <danivsainz@gmail.com>
2018-02-20 07:41:51 -0500

    add redirect from login auth dialog to maintain same functionality across site

--- auth-template/angular-src/src/app/components/dialogs/auth-dialog/auth-dialog.component.ts
+++ auth-template/angular-src/src/app/components/dialogs/auth-dialog/auth-dialog.component.ts
@@ -80,7 +80,7 @@ export class AuthDialogComponent implements OnInit{
         if(data.success) {
           this.authService.storeUserData(data.token, data.user);
           this.flashMessage.show('You are now logged in', {cssClass: 'alert-success', timeout: 5000});
-          // this.router.navigate(['dashboard']);
+          this.router.navigate(['dashboard']);
         } else {
           this.flashMessage.show(data.msg, {cssClass: 'alert-danger', timeout: 5000});
           this.router.navigate(['login']);
