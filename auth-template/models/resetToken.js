36965fb1d1a34f272d683e559de8f850cebfcf51
danivsainz <danivsainz@gmail.com>
2018-02-20 16:10:13 -0500

    Tested token expiration feature successfully next commit revert back to one hour

--- auth-template/models/resetToken.js
+++ auth-template/models/resetToken.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const ResetTokenSchema =  mongoose.Schema({
     _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     token: { type: String, required: true },
-    createdAt: { type: Date, required: true, default: Date.now, expires: 3600 }
+    createdAt: { type: Date, required: true, default: Date.now, expires:'15s' }
 });
 
 const Token = module.exports = mongoose.model('ResetToken', ResetTokenSchema);
