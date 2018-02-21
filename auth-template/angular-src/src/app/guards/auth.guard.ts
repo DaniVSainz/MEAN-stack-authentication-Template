5e18e37ea1fd49eefc540d19c106ef10cf086839
danivsainz <danivsainz@gmail.com>
2018-02-20 19:25:00 -0500

    Add routes to homepage, add flashmessage service to our guard

--- auth-template/angular-src/src/app/guards/auth.guard.ts
+++ auth-template/angular-src/src/app/guards/auth.guard.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-
+import { FlashMessagesService } from 'angular2-flash-messages';
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor (private authService:AuthService, private router:Router){
+  constructor (private authService:AuthService,
+               private router:Router,
+               private flashMessage:FlashMessagesService){
 
   }
 
@@ -12,6 +14,7 @@ export class AuthGuard implements CanActivate {
     if(this.authService.loggedIn()) {
       return true;
     } else {
+      this.flashMessage.show('You need to be logged in to access this area', {cssClass: 'alert-danger', timeout: 3000});
       this.router.navigate(['/']);
       return false;
     }
