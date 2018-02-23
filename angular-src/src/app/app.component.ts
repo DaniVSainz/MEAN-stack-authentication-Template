ae5c99545bead08282486df0a08318d6ff64aadf
danivsainz <danivsainz@gmail.com>
2018-02-23 17:02:56 -0500

    Now showing online messages

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  constructor(private flashMessage:FlashMessagesService){
+
+  }
+
+  networkStatusMessage(){
+    if (navigator.onLine){
+      this.flashMessage.show(`You're back online now` , {cssClass: 'alert-success', timeout: 3000});
+    } else {
+      this.flashMessage.show(`You're lost network connection` , {cssClass: 'alert-success', timeout: 3000});
+    }
+  }
+
+  ngOnInit(){
+    this.networkStatusMessage();
+    window.addEventListener('online' , this.networkStatusMessage);
+    window.addEventListener('offline', this.networkStatusMessage);
+  }
 }
