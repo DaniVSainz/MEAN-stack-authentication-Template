5615cf30a00a1ef8aa0a174615e01c03d5a5b2a6
danivsainz <danivsainz@gmail.com>
2018-02-23 21:39:09 -0500

    more code for service worker offline site functionality

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -8,21 +8,20 @@ import { FlashMessagesService } from 'angular2-flash-messages';
 })
 export class AppComponent implements OnInit {
 
+  networkOnline:Boolean = true;
+
+
   constructor(private flashMessage:FlashMessagesService){
-
-  }
-
-  networkStatusMessage(){
-    if (navigator.onLine){
-      this.flashMessage.show(`You're back online now` , {cssClass: 'alert-success', timeout: 3000});
-    } else {
-      this.flashMessage.show(`You're lost network connection` , {cssClass: 'alert-success', timeout: 3000});
-    }
   }
 
   ngOnInit(){
-    this.networkStatusMessage();
-    window.addEventListener('online' , this.networkStatusMessage);
-    window.addEventListener('offline', this.networkStatusMessage);
+    window.addEventListener('online' , ()=>{
+      this.flashMessage.show(`You're back online now` , {cssClass: 'alert-success', timeout: 3000});
+    });
+    window.addEventListener('offline', ()=>{
+      this.networkOnline=false;
+      this.flashMessage.show(`You've lost network connection` , {cssClass: 'alert-danger', timeout: 3000});
+    });
   }
+
 }
